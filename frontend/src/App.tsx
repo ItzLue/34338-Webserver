3c21ab8e672d1321889382385a47e4a24bf21ee3
Loui Nissen-Petersen <59645750+ItzLue@users.noreply.github.com>
2021-01-15 09:36:27 +0100

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -20,13 +20,13 @@ function App() {
     // Want to use async/await? Add the `async` keyword to your outer function/method.
     async function getData() {
         try {
-            const response = await axios.get('/user?ID=12345');
+            const response = await axios.get('http://localhost:5000/?motion');
             console.log(response);
         } catch (error) {
             console.error(error);
         }
     }
-
+    
     console.log(motion);
     console.log(sound);
     console.log(alarm);
